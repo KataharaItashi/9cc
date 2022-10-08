e8b664349763ba88f99b34f6deb3f709a555dfc9
Katahara Itashi <gonzaresu2sei@gmail.com>
2022-10-08 13:17:49 +0900

    エラーメッセージを改良しました

--- 9cc.c
+++ 9cc.c
@@ -25,11 +25,19 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
-// エラーを報告するための関数
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告するための関数
 // printfと同じ引数を取る
-void error(char *fmt, ...) {
+void error_at(char *loc, char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -48,7 +56,7 @@ bool consume(char op) {
 // それ以外の場合にはエラーを報告する。
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at(token->str,"'%c'ではありません", op);
   token = token->next;
 }
 
@@ -56,7 +64,7 @@ void expect(char op) {
 // それ以外の場合にはエラーを報告する。
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -99,7 +107,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(token->str, "トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -107,8 +115,11 @@ Token *tokenize(char *p) {
 }
 
 int main(int argc, char **argv) {
+  
+  user_input = argv[1];
+
   if (argc != 2) {
-    error("引数の個数が正しくありません");
+    error_at(token->str, "引数の個数が正しくありません");
     return 1;
   }
 
